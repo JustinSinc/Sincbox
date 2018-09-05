a41aab629540c39b58e7bf84ea8fe6a90f40eadf
Justin Sinc <jpsinkula@hotmail.com>
2018-09-05 01:38:37 -0500

    Update minecraft_backup.sh

--- scripts/minecraft_backup.sh
+++ scripts/minecraft_backup.sh
@@ -4,26 +4,30 @@
 # shell script best practice
 set -euo pipefail
 
-# the directory the server is stored in
+# set the directory the server is stored in
 server_name="minecraft_misfits"
 
-# the filename to use for backups
-filename="$server_name"_"$(date +%Y%m%d_%H%M).tgz"
+# set the filenames to use for backups
+new_filename="nightly_"$(date +%Y%m%d_%H)05.tgz""
+old_filename="nightly_"$(date --date="yesterday" +%Y%m%d_%H)05.tgz""
 
 # display backup file name
-echo "Backing up server to file "$filename"..."
+echo "Backing up server to file "$new_filename"..."
 
-# back up server files
-tar --exclude=plugins/dynmap/web/tiles -zcvf "$HOME/$filename" -C "$HOME/$server_name" .
+# back up server files, skipping the bulky dynmap tilesets
+tar --exclude=plugins/dynmap/web/tiles -zcvf "$HOME/$new_filename" -C "$HOME/$server_name" .
 
 # let the user know it finished
 echo "Backed up server to "$filename". Exiting..."
 
 # copy the file over to gate
-scp "$HOME/$filename" gate:
+scp "$HOME/$new_filename" gate:
 
 # copy the file from gate to the fallback server
-ssh gate "scp "$HOME/$filename" fallback:minecraft_misfits/nightly_"$(date +%Y%m%d_%H)"05.tgz"
+ssh gate "scp "$HOME/$new_filename" fallback:minecraft_misfits/"
 
 # delete the file from gate
-ssh gate "rm "$HOME/$filename""
+ssh gate "rm "$HOME/$new_filename""
+
+# delete the previous day's backup
+rm "$HOME/$old_filename"
