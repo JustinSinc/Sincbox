1f4d4540574341bcf42d5aa4224f6efcf7f9e49e
Seednode <jpsinkula@hotmail.com>
2019-03-28 16:59:26 -0500

    Update count.sh

--- minecraft/count.sh
+++ minecraft/count.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# view current list of online players
 lxc exec "$1" -- /bin/su mine -c "tmux send-keys -t minecraft 'list' Enter && sleep 1 && tac /home/mine/logs/latest.log | sed -e '/players online:/q'"
