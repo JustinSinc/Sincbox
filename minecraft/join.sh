17ae95096983b5cc5da5ef4a112049d4f9a63e2b
Seednode <jpsinkula@hotmail.com>
2019-03-28 17:04:16 -0500

    Update join.sh

--- minecraft/join.sh
+++ minecraft/join.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 # attach to server console
+
+# if an incorrect number of arguments are applied, display usage info
+# and then exit
+if [ "$#" -eq 1 ]; then
+  echo "Usage: join.sh <container>"
+  exit 1
+fi
+
+# attach to the tmux pane running the server console
 lxc exec "$1" -- /bin/su mine -c "tmux attach -t minecraft"
