23666f95344573fe6b9b2a8d5d1a9440fc0985ea
Seednode <jpsinkula@hotmail.com>
2019-03-28 17:03:05 -0500

    Update launch.sh

--- minecraft/launch.sh
+++ minecraft/launch.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 # start a server
+
+# if an incorrect number of arguments are applied, display usage info
+# and then exit
+if [ "$#" -eq 1 ]; then
+  echo "Usage: launch.sh <container>"
+  exit 1
+fi
+
+# launch the specified server
 lxc exec "$1" -- /bin/sh -c "/bin/su mine -c /home/mine/start.sh"
