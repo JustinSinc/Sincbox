bb226710b7d53ad92f8850b7d81d84e81c91d2a7
Seednode <jpsinkula@hotmail.com>
2019-03-28 17:03:49 -0500

    Update count.sh

--- minecraft/count.sh
+++ minecraft/count.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 # view current list of online players
+
+# if an incorrect number of arguments are applied, display usage info
+# and then exit
+if [ "$#" -eq 1 ]; then
+  echo "Usage: count.sh <container>"
+  exit 1
+fi
+
+# list online players in the server console, and retrieve the output from the server log 
 lxc exec "$1" -- /bin/su mine -c "tmux send-keys -t minecraft 'list' Enter && sleep 1 && tac /home/mine/logs/latest.log | sed -e '/players online:/q'"
