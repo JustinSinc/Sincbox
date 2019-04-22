b33edc78c6f0c7bac163cc2d8296095ded751083
Seednode <jpsinkula@hotmail.com>
2019-04-22 17:35:30 -0500

    Update setup.sh

--- scripts/wireguard/setup.sh
+++ scripts/wireguard/setup.sh
@@ -2,6 +2,21 @@
 ## script to set up a basic wireguard server
 # must be run as root or with sudo
 
+# set wan interface name
+wan_interface="eth0"
+
+# set wireguard tunnel interface name
+tun_interface="wg0"
+
+# set wireguard port
+port="51820"
+
+# set the vpn subnets
+subnetv4="172.0.0"
+maskv4="/24"
+subnetv6="fddf:562f:958e:5a4d"
+maskv6="/64"
+
 # install prerequisites
 apt update && apt install software-properties-common
 
@@ -14,28 +29,18 @@ apt update && apt install wireguard wireguard-dkms wireguard-tools
 # generate keys for wireguard server
 umask 077
 wg genkey | tee /etc/wireguard/privatekey | wg pubkey | tee /etc/wireguard/publickey
-wg genpsk | tee /etc/wireguard/presharedkey
 
-# set wan interface name
-wan_interface="eth0"
-
-# set wireguard tunnel interface name
-tun_interface="wg0"
-
-# set the first three octets of the vpn subnet
-subnet="172.21.0"
-
-# set variables for keys
+# store the private key in $privatekey for use in the config file
 privatekey="$(cat /etc/wireguard/privatekey)"
 
 # generate wireguard config file
 cat <<EOL >/etc/wireguard/"$tun_interface".conf
 [Interface]
 PrivateKey = $privatekey
-Address = $subnet.1/24
-ListenPort = 51820
-PostUp = iptables -A FORWARD -i $tun_interface -j ACCEPT; iptables -t nat -A POSTROUTING -o $wan_interface -j MASQUERADE
-PostDown = iptables -D FORWARD -i $tun_interface -j ACCEPT; iptables -t nat -D POSTROUTING -o $wan_interface -j MASQUERADE
+Address = $subnetv4.1$maskv4, $subnetv6::1$maskv6
+ListenPort = $port
+PostUp = iptables -A FORWARD -i $tun_interface -j ACCEPT; iptables -t nat -A POSTROUTING -o $wan_interface -j MASQUERADE; ip6tables -A FORWARD -i $tun_interface -j ACCEPT; ip6tables -t nat -A POSTROUTING -o $wan_interface -j MASQUERADE
+PostDown = iptables -D FORWARD -i $tun_interface -j ACCEPT; iptables -t nat -D POSTROUTING -o $wan_interface -j MASQUERADE; ip6tables -D FORWARD -i $tun_interface -j ACCEPT; ip6tables -t nat -D POSTROUTING -o $wan_interface -j MASQUERADE
 EOL
 
 # start wireguard
