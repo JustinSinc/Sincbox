3437861320b0b472e323a90431eeba5f12642540
Seednode <jpsinkula@hotmail.com>
2019-04-29 17:03:29 -0500

    Updated to match certgen.sh

--- scripts/wireguard/setup.sh
+++ scripts/wireguard/setup.sh
@@ -2,6 +2,9 @@
 ## script to set up a basic wireguard server
 # must be run as root or with sudo
 
+# fail in a sane manner
+set -euo pipefail
+
 # set wan interface name
 wan_interface="eth0"
 
@@ -11,11 +14,11 @@ tun_interface="wg0"
 # set wireguard port
 port="51820"
 
-# set the vpn subnets
-subnetv4="172.0.0"
-maskv4="/24"
-subnetv6="fddf:562f:958e:5a4d"
-maskv6="/64"
+# set the private subnets to be used
+subnetv4="10.0.0"
+maskv4="24"
+subnetv6="fdab:1a11:7de6:5b40"
+maskv6="64"
 
 # install prerequisites
 apt update && apt install software-properties-common
@@ -37,7 +40,7 @@ privatekey="$(cat /etc/wireguard/privatekey)"
 cat <<EOL >/etc/wireguard/"$tun_interface".conf
 [Interface]
 PrivateKey = $privatekey
-Address = $subnetv4.1$maskv4, $subnetv6::1$maskv6
+Address = $subnetv4.1/$maskv4, $subnetv6::1/$maskv6
 ListenPort = $port
 PostUp = iptables -A FORWARD -i $tun_interface -j ACCEPT; iptables -t nat -A POSTROUTING -o $wan_interface -j MASQUERADE; ip6tables -A FORWARD -i $tun_interface -j ACCEPT; ip6tables -t nat -A POSTROUTING -o $wan_interface -j MASQUERADE
 PostDown = iptables -D FORWARD -i $tun_interface -j ACCEPT; iptables -t nat -D POSTROUTING -o $wan_interface -j MASQUERADE; ip6tables -D FORWARD -i $tun_interface -j ACCEPT; ip6tables -t nat -D POSTROUTING -o $wan_interface -j MASQUERADE
