c72236c101e18065631a99c2a3d57e968c14e67f
Seednode <github@seedno.de>
2019-08-29 20:48:29 -0500

    Update update_bitwarden.sh

--- scripts/update_bitwarden.sh
+++ scripts/update_bitwarden.sh
@@ -13,14 +13,14 @@ docker rm bitwarden
 sudo docker run -d      --name bitwarden \
                         --restart always \
                         --user 1001 \
-                        -e DOMAIN=https://bw.seednode.co \
+                        -e DOMAIN=https://bw.seedno.de \
                         -e WEBSOCKET_ENABLED=true \
                         -e SIGNUPS_ALLOWED=false \
                         -e INVITATIONS_ALLOWED=false \
                         -e SHOW_PASSWORD_HINT=false \
                         -e LOG_FILE=/data/bitwarden.log \
                         -e ROCKET_PORT=8080 \
-                        -v /bw-data/:/data/ \
-                        -p 8080:8080 \
+                        -v /opt/bw-data/:/data/ \
+                        -p 8090:8080 \
                         -p 3012:3012 \
                         mprasil/bitwarden:latest
